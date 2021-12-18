13497deb80f4ff7af3ed99fbfc98b1672acd613e
qianye <972781783@qq.com>
2021-12-18 10:24:31 +0800

    更新项目结构

---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-menu
+        style="width:200px;min-height: calc(100vh - 50px)"
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+    >
+      <el-menu-item index="1">
+        <el-icon><location /></el-icon>
+        <span>导航一</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <el-icon><document /></el-icon>
+        <span>导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <span>导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import {
+  Location,
+  Document,
+  Menu as IconMenu,
+  Setting,
+} from '@element-plus/icons-vue'
+
+export default {
+  name: "Aside",
+  components:{
+    Location,
+    Document,
+    IconMenu,
+    Setting,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <div style="padding: 10px">
+    <!--    功能区域-->
+    <div style="margin: 10px 0">
+      <el-button type="primary">新增</el-button>
+      <el-button type="primary">导入</el-button>
+      <el-button type="primary">导出</el-button>
+    </div>
+    <!--    搜索区域-->
+    <div style="margin: 10px 0">
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"></el-input>
+      <el-button type="primary" style="margin-left: 10px">查询</el-button>
+    </div>
+    <el-table :data="tableData" border stripe style="width: 100%;">
+      <el-table-column prop="date" label="Date" sortable=""/>
+      <el-table-column prop="name" label="Name"/>
+      <el-table-column prop="address" label="Address"/>
+      <el-table-column fixed="right" label="操作">
+        <template #default="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index,scope.row)">编辑</el-button>
+          <el-popconfirm title="确定删除吗?">
+            <template #reference>
+              <el-button size="mini" type="danger" @click="handleDelete(scope.$index,scope.row)">删除</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div style="margin:10px 0">
+      <el-pagination
+          v-model:currentPage="currentPage"
+          :page-sizes="[5, 10, 20]"
+          :page-size="10"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+      >
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      currentPage:1,
+      total:10,
+      search: '',
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ],
+    }
+  },
+  methods:{
+    handleEdit(){},
+    handleSizeChange(){},
+    handleCurrentChange(){},
+    handleDelete(){}
+  }
+}
+</script>
